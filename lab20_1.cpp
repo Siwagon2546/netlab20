a3746c0f128ec5e0b7a437278261b72d9a686652
Siwagon2546 <netevgjqw@gmail.com>
2023-02-21 16:39:06 +0700

    lab20_1.cpp complete

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
+
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
+double overlap(Rect R1,Rect R2){
+    double X1=max(R1.x,R2.x);
+    double X2=min(R1.x+R1.w,R2.x+R2.w);
+    double y1=min(R1.y,R2.y);
+    double y2=max(R1.y-R1.h,R2.y-R2.h);
+    
+    double area = (X2-X1)*abs(y1-y2);
+    if(area<0){
+        return 0;
+    }else{
+        return area;
+    }
 
 }
